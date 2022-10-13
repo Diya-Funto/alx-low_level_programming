01aec78cdef2bff03dac74a26da9cf7400248628
Diya-Funto <funto.s@yahoo.com>
2022-10-13 14:31:20 -0700

    more changes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ int main(void)
 	long long int ll;
 	float f;
 
-	printf("Size of char: %zu bytes(s)\n", sizeof(c));
-	printf("Size of int: %zu bytes(s)\n", sizeof(i));
-	printf("Size of long int: %zu bytes(s)\n", sizeof(l));
-	printf("Size of long long int: %zu bytes(s)\n", sizeof(ll));
-	printf("Size of float: %zu bytes(s)\n", sizeof(f));
+	printf("Size of char: %zu byte(s)\n", sizeof(c));
+	printf("Size of int: %zu byte(s)\n", sizeof(i));
+	printf("Size of long int: %zu byte(s)\n", sizeof(l));
+	printf("Size of long long int: %zu byte(s)\n", sizeof(ll));
+	printf("Size of float: %zu byte(s)\n", sizeof(f));
 
 	return (0);
 }
