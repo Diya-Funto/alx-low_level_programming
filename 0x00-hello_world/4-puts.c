cbccf7caab797c96beed643d57d117bcdb826283
Diya-Funto <funto.s@yahoo.com>
2022-10-13 13:55:53 -0700

    more changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - Entry
+ * main- Entry point
  *
- * return -when successful 0
+ * Return: Successful 0
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
