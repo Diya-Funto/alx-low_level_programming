8c3cde662c85cba4c6e2bddd84f231ad5977d398
Diya-Funto <funto.s@yahoo.com>
2022-10-15 08:14:17 -0700

    more changes

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
  * main - Entry point
@@ -11,10 +11,10 @@ int main(void)
 	char alphabet;
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-		putchar(alphabet);
-	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
-		putchar(alphabet);
-	putchar('\n')
+		if (alphabet != 'q' && 'e')
+			putchar(alphabet);
+	putchar('\n');
 
 	return (0);
 }
+
