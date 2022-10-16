3ed28e682d913bd08ff5e3df25d0878769b7a8f0
Diya-Funto <funto.s@yahoo.com>
2022-10-16 09:15:02 -0700

    more changes

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-	int digit;
+	int ch;
 
-	for (digit = 0; digit < 10; digit++)
-		putchar (digit);
+	for (ch = 0; ch < 10; ch++)
+		putchar (ch);
 	putchar('\n');
 
 	return (0);
