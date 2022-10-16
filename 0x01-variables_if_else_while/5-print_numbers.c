7183b40e862fe1b194adf746513982445c3795f8
Diya-Funto <funto.s@yahoo.com>
2022-10-16 08:48:56 -0700

    more changes

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: When successful 0
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+		putchar(digit);
+	putchar('\n');
+
+	return (0);
+}
