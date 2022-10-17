68e24a5c64acc6ae697c14b1db3f32fd78849e4d
Diya-Funto <funto.s@yahoo.com>
2022-10-16 23:06:25 -0700

    more changes

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,4 @@
 #include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
 
 /**
  * main - Entry point
@@ -12,7 +10,7 @@ int main(void)
 {
 	int digit;
 
-	for (digit = 0; digit < 10; digit++)
+	for (digit = '0'; digit <= '9'; digit++)
 		putchar (digit);
 	putchar('\n');
 
