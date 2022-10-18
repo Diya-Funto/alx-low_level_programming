d75b51e5c4a1bee23f343b013045d3077e57ed78
Diya-Funto <funto.s@yahoo.com>
2022-10-18 14:55:32 -0700

    more changes

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints lower case alphabet
+ */
+
+void print_alphabet(void)
+{
+
+	char lcase;
+
+	for (lcase = 'a'; lcase <= 'z'; lcase++)
+		_putchar(lcase);
+
+	_putchar('\n');
+
+}
+
