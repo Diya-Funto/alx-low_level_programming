7826de271d9d27342353e1a80de74c7a4b84bbd4
Diya-Funto <funto.s@yahoo.com>
2022-10-19 15:05:26 -0700

    more changes

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - sums integers
+ * @n: first integer to be added
+ * @b: second integer to be added
+ *
+ * Return: sum of integers
+ */
+
+int add(int n, int b)
+{
+	return (n + b);
+}
