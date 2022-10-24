c4c6ae4a500083a021818103cbb823012f3efaaa
Diya-Funto <funto.s@yahoo.com>
2022-10-24 09:59:41 -0700

    Initial commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * rest_to_98 - takes a pointer and update the value it points to
+ * @n: character to update 
+ *
+ * Return: When successful 0
+ */
+
+void reset_to_98(int *n)
+{
+	int *n;
+
+	*n = 98;
+	_putchar(*n);
+	_putchar('\n');
+}
