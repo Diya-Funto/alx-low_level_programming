5f75eb088767ef426afef4923902c2922fa18400
Diya-Funto <funto.s@yahoo.com>
2022-10-24 10:07:56 -0700

    Initila commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- * rest_to_98 - takes a pointer and update the value it points to
- * @n: character to update 
+ * reset_to_98 - takes a pointer and update the value it points to
+ * @n: character to update
  *
  * Return: When successful 0
  */
 
 void reset_to_98(int *n)
 {
-	int *n;
 
 	*n = 98;
 	_putchar(*n);
