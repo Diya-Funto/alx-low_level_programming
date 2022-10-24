34251ebbc7f2e5b3e2e1828602cfd355a257353b
Diya-Funto <funto.s@yahoo.com>
2022-10-24 11:18:57 -0700

    more changes

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ *
+ * Return: string length.
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	for (len = 0; *s != '\0'; ++len)
+		return (len);
+}
