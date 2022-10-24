e177c47d3dfeef0dc0039a5b3cfe6b7c7c2b680c
Diya-Funto <funto.s@yahoo.com>
2022-10-24 10:43:26 -0700

    more changes

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the vlaue of two integers
+ * @a:  first integer
+ * @b: second integer
+ *
+ * Return: When successful 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = *a + *b;
+	*b = *a - *b;
+	*a = *a - *b;
+	_putchar(*a);
+	_putchar(*b);
+	_putchar('\n');
+}
