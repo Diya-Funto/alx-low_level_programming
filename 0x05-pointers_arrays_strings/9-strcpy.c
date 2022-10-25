98a56b92a9354d483e7b8586508a1419461fc098
Diya-Funto <funto.s@yahoo.com>
2022-10-25 06:58:36 -0700

    more changes

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * strcpy - string copy
+ * @dest: pointer to buffer
+ * @scr: pointer to a string
+ *
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src);
+{
+
