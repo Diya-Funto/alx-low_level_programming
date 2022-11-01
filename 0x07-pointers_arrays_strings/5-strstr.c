d50caf472214c591dd3f2c8e164266404e90dd9b
Diya-Funto <funto.s@yahoo.com>
2022-11-01 13:58:14 -0700

    more changes

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strstr - locates a substring
+ * @haystack: pointer to source string 
+ * @needle: pointer to comparative string
+ * Return: pointer to the beginning of the located substring
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
