3b044c7395391e78eb2481cf6331b5d5d95532df
Diya-Funto <funto.s@yahoo.com>
2022-11-04 10:26:52 -0700

    Initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: On success, 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	exit(EXIT_SUCCESS);
+}
