fe3b3540750bd6ac05eae95df3505ce99ef6780d
Diya-Funto <funto.s@yahoo.com>
2022-11-04 10:39:12 -0700

    Initial commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
+ * main - print program name
  * @argc: argument count
  * @argv: argument vector
  * Return: On success, 0
@@ -7,6 +9,7 @@
 
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
-	exit(EXIT_SUCCESS);
+	while (argc--)
+		printf("%s\n", argv[0]);
+	return (0);
 }
