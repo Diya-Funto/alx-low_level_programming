d846fd0e285164655966fce2f519aab69090d6c8
Diya-Funto <funto.s@yahoo.com>
2022-11-15 13:44:59 -0800

    more changes

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -18,6 +18,8 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		if (d->age <= 0)
 			printf("Age: (nil)\n");
+		else
+			printf("Age: %lf\n", d->age);
 		if (d->owner == NULL)
 			printf("Name = (nil)\n");
 		else
