db105868c45def5b2e3a064fe1dcdc86a26b233c
Diya-Funto <funto.s@yahoo.com>
2022-11-18 11:48:10 -0800

    more changes

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -24,10 +24,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%d", va_arg(args, int));
 
 		if (i < (n - 1))
-			printf("%s ", separator);
+			printf("%s", separator);
 	}
 
-	printf("\n");
-
 	va_end(args);
+	printf("\n");
 }
