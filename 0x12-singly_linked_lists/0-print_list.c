bedf61f2934e2e9008b695d9d4ae8757b63ef40f
Diya-Funto <funto.s@yahoo.com>
2022-11-25 12:10:25 -0800

    Initial commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * print_list - Prints all the elements of a list_t list
+ * @h: Pointer to head of list
+ *
+ * Return: Nodes
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *s = h;
+	size_t i = 0;
+
+	if (s == NULL)
+		return (0);
+
+
+	while (s != NULL)
+	{
+		if (s->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", s->len, s->str);
+		}
+		s = s->next;
+		i++;
+	}
+	return (i);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+How to use use linked lists
